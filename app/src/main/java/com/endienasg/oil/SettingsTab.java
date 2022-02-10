91e21942f43a25e451e129cfbf2ccd0b1b539cde
Enzer <mksprotopopov@gmail.com>
2022-02-10 15:34:30 +0900

    Еще коммент

--- app/src/main/java/com/endienasg/oil/SettingsTab.java
+++ app/src/main/java/com/endienasg/oil/SettingsTab.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class SettingsTab extends Fragment {
     @Override
@@ -15,4 +16,5 @@ public class SettingsTab extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings_tab, container, false);
     }
+
 }
